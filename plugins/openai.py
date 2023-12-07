e439cc734df0e6d54f85d448ff2d4aa914eecbfb
Naruto46i <150582863+Naruto46i@users.noreply.github.com>
2023-12-07 19:16:30 +0530

    Update openai.py

--- plugins/openai.py
+++ plugins/openai.py
@@ -1,4 +1,4 @@
-from info import SUPPORT_GROUP, SUPPORT_LINK, OPENAI_API
+from info import OPENAI_API
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import openai
@@ -7,9 +7,10 @@ openai.api_key = OPENAI_API
 
 @Client.on_message(filters.command("openai"))
 async def ask_question(client, message):
+    try:
         text = message.text.split(" ", 1)[1]
     except:
-        return await message.reply_text("Give an input!")
+        return await message.reply_text("Command Incomplete!\nUsage: /openai your_question")
     msg = await message.reply("Searching...")
     try:
         response = openai.Completion.create(
@@ -20,3 +21,14 @@ async def ask_question(client, message):
         await msg.edit(f"User: {message.from_user.mention}\nQuery: <code>{text}</code>\n\nResults:\n\n<code>{response.choices[0].text}</code>")
     except Exception as e:
         await msg.edit(f'Error - <code>{e}</code>')
+   
+ 
+     
+            
+  
+   
+    
+         
+            
+        
+       
